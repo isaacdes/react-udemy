592bd473ce96bd0f350d55a15d5367ce2ffb8f2b
Isaac Xavier Desouza <66382402+isaacdes@users.noreply.github.com>
2022-05-10 13:40:50 +0530

    styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,21 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css'
+
 function ExpenseItem(props) {
   //   const expenseDate = new Date(2022, 4, 6);
   //   const expenseTtile = "Car Inusrance";
   //   const expenseAmount = 200;
 
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
+ 
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div> {year} </div>
-        <div> {day} </div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount} </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
